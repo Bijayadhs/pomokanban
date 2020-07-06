b738734ed03d00c7f09323b423832d2714a524f9
Bijayadhs <bijayadhikari107@gmail.com>
2020-07-06 14:36:47 +1000

    created model and script to add and remove button

--- css_pk/script.js
+++ css_pk/script.js
@@ -1,1 +1,14 @@
-alert('start');
+const addTask = document.getElementById('addTask')
+const addModel = document.getElementById('addModel')
+const closeBtn = document.getElementById('closeBtn')
+
+addTask.addEventListener('click',openModel)
+closeBtn.addEventListener('click',closeModel)
+
+function openModel(){
+    addModel.classList.add('show')
+}
+
+function closeModel(){
+    addModel.classList.remove('show')
+}
